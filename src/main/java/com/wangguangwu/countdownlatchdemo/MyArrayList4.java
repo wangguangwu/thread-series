7cead05c98d789f708520f2f443d3158c8ec371b
wangguangwu <wangguangwu@pukanghealth.com>
2022-07-01 10:50:15 +0800

    save

--- src/main/java/com/wangguangwu/countdownlatchdemo/MyArrayList4.java
+++ src/main/java/com/wangguangwu/countdownlatchdemo/MyArrayList4.java
@@ -1,4 +1,4 @@
-package com.wangguangwu.cpuntdownlatchdemo;
+package com.wangguangwu.countdownlatchdemo;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
---
+++ src/main/java/com/wangguangwu/volatiledemo/Singleton.java
@@ -0,0 +1,17 @@
+package com.wangguangwu.volatiledemo;
+
+/**
+ * 线程安全的单例模式
+ *
+ * @author wangguangwu
+ */
+public class Singleton {
+
+    /**
+     * 私有化构造方法，避免从
+     */
+    private Singleton() {
+    }
+
+
+}
